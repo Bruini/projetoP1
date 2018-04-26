e5465b0e869355f6cf89c70d479c72b7355d0216
Pedro Bruini <pedro.bruini@hotmail.com>
2018-04-26 01:36:08 -0300

    Formulario

---
+++ php/envio.php
@@ -0,0 +1,19 @@
+<?php
+$nome = $_POST['nome'];
+echo $nome;
+echo "<br/>";
+$email = $_POST['email'];
+echo $email;
+echo "<br/>";
+$cpf = $_POST['cpf'];
+echo $cpf;
+echo "<br/>";
+$numero = $_POST['numero'];
+echo $numero;
+echo "<br/>";
+$bandeira = $_POST['bandeira'];
+echo $bandeira;
+echo "<br/>";
+$validade = $_POST['validade'];
+echo $validade;
+?>
